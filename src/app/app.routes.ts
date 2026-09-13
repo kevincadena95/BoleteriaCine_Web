776b8396e7ff7ae832eaa2a2a3fc77296d8109c7
Matheew <arielmathew2607@gmail.com>
2026-09-13 00:10:42 -0500

    Feat: Implementacion de la pantalla de dulceria y carga de imagenes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Cartelera } from './cartelera/cartelera';
 import { Dulceria } from './dulceria/dulceria';
 
 export const routes: Routes = [
-    {path:'', component: Cartelera},
-    {path:'dulceria', component: Dulceria},
-    {path:'**', component: Cartelera},
+    { path: '', component: Cartelera },
+    { path: 'cartelera', component: Cartelera },
+    { path: 'dulceria', component: Dulceria },
+    { path: '**', redirectTo: 'cartelera' }
 ];
